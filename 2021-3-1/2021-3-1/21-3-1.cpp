30da7614de29157bb9bedcff45d67f2af4bbcb14
fenhongpai <835749378@qq.com>
2021-03-01 22:25:13 +0800

    55

---
+++ 2021-3-1/2021-3-1/21-3-1.cpp
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if ((year %4== 0) && (year % 100 != 0) || (year % 400 == 0))
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//		/*if (year % 4 == 0 && year % 100 != 0)
+//		{
+//			printf("%d", year);
+//			count++;
+//		}
+//		else if (year % 400 == 0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}*/
+//	/*}*/
+//	printf("\ncount=%d", count);
+//	return 0;
+//}
+//{/*
+//	int m = 0;
+	/*int n = 0;
+	int r = 0;
+	scanf("%d%d", &m, &n);
+	while (m%n)
+	{
+		r = m%n;
+		m = n;
+		n = r;
+	}
+	printf("%d\n", n);
+	return 0;
+}*/
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 3 == 0)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+//{
+//	int x = 3;
+//	int y = 3;
+//	switch (x % 2)
+//	{
+//	case 0:
+//		printf("first");
+//	case 1:
+//		printf("second");
+//	default:printf("hello");
+//	
+//	case 2:
+//		printf("third");
+//	}
+//return 0;
+//}
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//
