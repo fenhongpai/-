47de8ac6ed753b09a479cb85524a4512c69544ed
fenhongpai <835749378@qq.com>
2021-03-08 22:45:27 +0800

    2

---
+++ 2021-3-8/2021-3-8/源.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//	a = a + 2;
+//}
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	scanf("%d", &num);
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (1 == ((num >> i) & 1))
+//			count++;
+//	}
+//	printf("%d\n", count);
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	a = a^b;
+//	b = a^b;
+//	a = a^b;
+//	printf("a=%d b=%d", a, b);
+//}
+//int main()
+//{
+	/*int a = 3;
+	int b = 5;
+	int c = a&b;*/
+	/*int a = 3;
+	int b = 5;
+	int c = a^b;*/
+	/*int c = a | b;*/
+//	printf("%d\n", c);
+//}
+//int main()
+//{
+//	int a = -1;
+//	int b = a >> 1;
+//	printf("%d\n", b);
+//}
+//int main()
+//{
+//	int a = 16;
+//	int b = a >> 1;
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	double a = 5 / 2.0;
+//	printf("a=%lf\n", a);
+//	return 0;
+//}
